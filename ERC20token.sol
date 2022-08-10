34db4edd4b816b9e301fef9fbc530f9edc976c76
odionMercy8 <oseiweifebhor@gmail.com>
2022-08-10 03:45:14 +0100

    second commit

--- ERC20token.sol
+++ ERC20token.sol
@@ -13,7 +13,7 @@ interface IERC20 {
     function approve(address spender, uint amount) external returns (bool);
 
     function transferFrom(
-        address sender,
+        address sender,  
         address recipient,
         uint amount 
     ) external returns (bool);
@@ -41,7 +41,7 @@ contract ERC20 is  IERC20{
         return balance[tokenOwner];
     }
         
-    function transfer(address to, uint amount) public override returns(bool success){
+    function transfer(address to, uint amount) public override virtual returns(bool success){
         require(balance[msg.sender] >= amount, "not enough token");
         balance[to] = balance[to] + amount;
         balance[msg.sender] = balance[msg.sender] - amount;
@@ -67,7 +67,7 @@ contract ERC20 is  IERC20{
     // }
 
 
-    function transferFrom(address sender,address recipient, uint token) external override returns(bool){
+    function transferFrom(address sender,address recipient, uint token) public override virtual returns(bool){
         require(allowed[sender][recipient] >= token, "not allowed");
         require(balance[sender] >= token, "insufficient" );
         balance[sender] = balance[sender] - token;
@@ -77,5 +77,98 @@ contract ERC20 is  IERC20{
     }
 }
 
-///this is inheriting the interface soo you have  to override it
+///this is inheriting the interface soo you can to override it or just implement it like that 
+    
+
+contract ICO is ERC20{
+    address public manger;
+    address public desposit;
+    uint tokenPrice = 0.1 ether ;
+    uint TokenCap = 300;
+    uint amountRaised ;
+    uint tradeTime = Icoend + 3600;
+    uint minPrice = 10 ether;
+    uint maxPrice = 0.1 ether;
+    uint IcoStart  = block.timestamp;
+    uint Icoend = block.timestamp + 120;
+
+    enum status {Bresumed,Aresumed, ruuning, end}
+
+    event investLog(address investor, uint amount, uint NoToken);
+
+    status public AllStatus;
+
+    constructor(address payable _desposit){
+        desposit = _desposit;
+        manger = msg.sender;
+         AllStatus = status.Bresumed;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == manger, "not manger");
+        _;
+    }
+   
+    function resume() external onlyOwner{
+        AllStatus = status.ruuning;
+    }
+
+    function halted() external onlyOwner{
+        AllStatus = status.end;
+    } 
+
+    function ChangeDesposit(address payable _Cdesposit) external onlyOwner{
+        desposit = _Cdesposit;
+    }  
+
+    function getState() public view returns(status){
+        if(AllStatus == status.end){
+            return status.end;
+        }else if(block.timestamp < IcoStart){
+            return status.Bresumed;
+        }else if(block.timestamp >= IcoStart && block.timestamp <= Icoend){
+            return status.ruuning;
+        }else{
+            return status.Aresumed;
+        }
+    }
+
+    function invest() public payable returns(bool){
+        AllStatus = getState();
+        require(AllStatus == status.ruuning, "not in the right state");
+        require(TokenCap >= msg.value, "no longer in section");
+        amountRaised = amountRaised + msg.value;
+        require(msg.value >= minPrice && maxPrice <= msg.value, "not in range to invest");
+        uint amountToken = msg.value/tokenPrice;
+        balance[msg.sender] + amountToken;
+        balance[founder] - amountToken;
+        (bool sent,) = payable(desposit).call{value: msg.value}("");
+        emit investLog(msg.sender, msg.value, amountToken);
+        return sent;
+    }
+
+    function burn() external returns(bool){
+        AllStatus = getState();
+        require(AllStatus == status.Aresumed, "finished the distribution");
+        balance[founder] = 0;
+        return true;
+    } 
+
+    function transfer(address to ,uint tokens) public override returns(bool success){
+        require(block.timestamp > tradeTime ,"not now");
+        super.transfer(to, tokens);  //can use either super which refers to the parent contract or the name of the contract
+        return success;
+    } 
+
+    function transferFrom(address owner,address spender, uint token) public override returns(bool){
+        require(block.timestamp > tradeTime ,"not now");
+        super.transferFrom(owner, spender, token);
+        return true;
+    }
+
+    receive() external payable{
+        invest();
+    }
+
+} 
 
